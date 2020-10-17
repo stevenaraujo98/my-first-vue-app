c05ba1144b540c7c6f548404b6c0f72a2418832d
Steven Araujo <steven.araujo@hotmail.com>
2020-10-16 23:27:27 -0500

    Creacion de componente, variables y uso de roter

---
+++ src/views/Contact.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Contacto</h1>
+
+        <Pokemon/>
+        <button @click="changeContador">Aumentar {{contador}}</button>
+
+        <br/>
+        <br/>
+
+        {{"Tu mensaje es "+mensaje}}
+        <form>
+            <input type="email" v-model="mensaje"/>
+        </form>
+    </div>
+</template>
+
+<script>
+import Pokemon from '@/components/Pokemon.vue';
+export default {
+    components:{
+        Pokemon
+    },
+    data(){
+        return {
+            contador: 0,
+            mensaje: ''
+        }
+    },
+    methods:{
+        changeContador(){
+            this.contador++;
+        }
+    },
+    mounted() {
+        console.log("Aqui se haria uso de axios para fetch o ajax");
+    }
+}
+
+</script>
